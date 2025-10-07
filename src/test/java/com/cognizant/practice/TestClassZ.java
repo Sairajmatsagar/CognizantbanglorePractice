a1869cfa8cd2741bcebf834b74d8642485e028ea
Sairajmatsagar <sairajmatsagar123@gmail.com>
2025-10-07 09:40:02 +0530

    gaytri joined commit

---
+++ src/test/java/com/cognizant/practice/TestClassZ.java
@@ -0,0 +1,12 @@
+package com.cognizant.practice;
+
+import org.testng.annotations.Test;
+
+public class TestClassZ {
+	
+	@Test
+	public void testZ() {
+		System.out.println("this is test z Gayatri Joined class");
+	}
+
+}
