9ee3053b86d5f02aa060ffd053106f794a403e78
Sairajmatsagar <sairajmatsagar123@gmail.com>
2025-10-07 08:22:57 +0530

    hi 1st commit

---
+++ src/test/java/com/cognizant/practice/Test1.java
@@ -0,0 +1,17 @@
+package com.cognizant.practice;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	
+	@Test
+	public void testpractice1() {
+		System.out.println("Test 1 for sample");
+	}
+	
+	@Test
+	public void testpractice2() {
+		System.out.println("Test 2 for sample");
+	}
+}
