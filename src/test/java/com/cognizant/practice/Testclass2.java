8431e719cbc47ed02330c06f87a0a9b72b444b60
Sairajmatsagar <sairajmatsagar123@gmail.com>
2025-10-07 08:50:40 +0530

    commit 4

---
+++ src/test/java/com/cognizant/practice/Testclass2.java
@@ -0,0 +1,13 @@
+package com.cognizant.practice;
+
+import org.testng.annotations.Test;
+
+public class Testclass2 {
+
+	
+	
+	@Test
+	public void test3() {
+		System.out.println("This is test 3");
+	}
+}
